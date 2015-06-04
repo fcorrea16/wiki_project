eb56f911ce327d6e862e1393e092a642adbb03aa
Dennis Liaw <dennis.liaw@gmail.com>
2015-06-04 13:46:57 -0400

    sets up db

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,31 @@
-DROP DATABASE IF EXISTS wiki;
-
-CREATE DATABASE wiki;
-\c wiki
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS articles;
 
 CREATE TABLE authors (
-	id SERIAL PRIMARY KEY, 
-	fname VARCHAR NOT NULL,
-	lname VARCHAR NOT NULL, 
-	email VARCHAR NOT NULL,
-	password VARCHAR NOT NULL,
-	bio VARCHAR
+	id 			 SERIAL   PRIMARY KEY,
+	fname 	 VARCHAR  NOT NULL,
+	lname 	 VARCHAR  NOT NULL,
+	email 	 VARCHAR  NOT NULL,
+	password VARCHAR  NOT NULL,
+	bio 		 VARCHAR
 );
 
 CREATE TABLE categories (
-	id SERIAL PRIMARY KEY,
-	cat_name VARCHAR NOT NULL, 
-	summary VARCHAR
+	id 	 SERIAL  PRIMARY KEY,
+	name VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE articles (
-	id SERIAL PRIMARY KEY, 
-	name VARCHAR NOT NULL, 
-	content TEXT,
-	created_at TIMESTAMP NOT NULL, 
-	edited_on TIMESTAMP NOT NULL,
-	category_id INTEGER NOT NULL,	
-	author_id INTEGER NOT NULL 
+	id 					SERIAL 		PRIMARY KEY,
+	name 				VARCHAR 	NOT NULL,
+	content 		TEXT 			NOT NULL,
+	created_at 	TIMESTAMP NOT NULL,
+	edited_on 	TIMESTAMP NOT NULL,
+	category_id INTEGER 	REFERENCES categories(id),
+	author_id 	INTEGER 	REFERENCES authors(id)
 );
+
 
 -- REFERENCES categories(id),
 -- REFERENCES authors(id)
